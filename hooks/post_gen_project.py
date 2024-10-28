ea305252ba207bcd25272a43f435dd902cc623d6
Matthew Leingang <leingang@nyu.edu>
2024-10-28 10:10:16 -0400

    Add an output message which suggests next steps

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,6 @@
+
+if (__name__ == '__main__'):
+    print("Document module successfully generated. You may want to run:")
+    print("    git add . ")
+    print("    git commit -m 'Add stub for {{ cookiecutter.exam_name }}' ")
+    print("    pushd {{ cookiecutter.exam_code }} && l3build tag 0.0 && popd ")
